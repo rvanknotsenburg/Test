e1a2a2cafbaaf44101d58718fe259839418f0203
Remco <rvanknotsenburg@circlepartners.com>
2021-02-01 16:10:38 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$name = 'Remco';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Remco's test site</title>
+</head>
+<body>
+
+<h1>This is a Heading </h1>
+<h3> this is the second heading</h3>
+<p>Mijn naam is: <?= $name; ?> verder is het geen probleem.
+  <br> Dus check dit </br>
+<p> this is another paragraph</p>
+<p> and another</p>
+<a href="'https://www.outlook.com">outlook.com </a>
+
+
+
+</body>
+</html>
+
+
